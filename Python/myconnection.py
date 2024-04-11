513a61b68b58dc64d51514e4aa374186c71c74ec
villepohjonen <37143875+villepohjonen@users.noreply.github.com>
2024-04-11 15:22:40 +0300

    Add files via upload

--- Python/myconnection.py
+++ Python/myconnection.py
@@ -1,40 +1,14 @@
-import logging
-import time
-import mysql.connector
+import plotly
+plotly.__version__
+import chart_studio.plotly as py
+import plotly.graph_objs as go
 
+import MySQLdb
+import pandas as pd
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+conn = MySQLdb.connect(host="localhost", user="DESKTOP-OPATNA9\admin", passwd="", db="results.dbo")
+cursor = conn.cursor()
+cursor.execute('select date, home_team, away_team, home_score, away_score, tournament, city, country');
 
-
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-
-file_handler = logging.FileHandler("cpy-errors.log")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler) 
-
-def connect_to_mysql(config, attempts=3, delay=2):
-    attempt = 1
-    # Implement a reconnection routine
-    while attempt < attempts + 1:
-        try:
-            return mysql.connector.connect(**config)
-        except (mysql.connector.Error, IOError) as err:
-            if (attempts is attempt):
-                # Attempts to reconnect failed; returning None
-                logger.info("Failed to connect, exiting without a connection: %s", err)
-                return None
-            logger.info(
-                "Connection failed: %s. Retrying (%d/%d)...",
-                err,
-                attempt,
-                attempts-1,
-            )
-            # progressive reconnect delay
-            time.sleep(delay ** attempt)
-            attempt += 1
-    return None
+rows = cursor.fetchall()
+str(rows)[0:300]
